1e13d45161266cd3783556c44c02dc985547ef3c
alex-yan <114714712+chaoxiyan1225@users.noreply.github.com>
2025-03-27 17:37:24 +0800

    Create tweet_conf.py

---
+++ twitter/tweet_conf.py
@@ -0,0 +1,14 @@
+user_ids = [2244994945, 6253282]
+
+# kol  bio 关键词: 如果用户已经是按照上述的user_ids指定，该位置可以不设置
+bio_keys = ["crypto", "cryptocurrency", "blockchain", "web3", "NFT", "DeFi", "bitcoin", "BTC", "ethereum", "ETH","token", "trader", "investor", "币圈", "加密货币" ]
+
+#crypto  web3 关键词
+Crypto_keys = ["crypto", "cryptocurrency", "bitcoin", "BTC", "ethereum", "ETH", "blockchain", "token", "NFT", "DeFi", "decentralized", "coin"]
+Web3_keys = ["web3","dapp", "dao", "metaverse", "smart", "contract", "wallet"]
+crypto_web3_keys = Crypto_keys + Web3_keys
+
+# 美股 宏观环境关键词
+stock_keys= ["stock", "NASDAQ", "NYSE", "SP500", "Dow Jones", "bull market", "bear market", "IPO", "earnings", "fed", "interest rate", "inflation", "recession, macro"]
+macroen_keys = ["economy", "GDP", "unemployment", "trade", "policy", "fiscal", "monetary"]
+stock_macroen_keys = stock_keys + macroen_keys
