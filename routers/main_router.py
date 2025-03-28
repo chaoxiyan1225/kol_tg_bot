185112cd7d6c5c92c32b1e2e535cb520ec37969d
alex-yan <114714712+chaoxiyan1225@users.noreply.github.com>
2025-03-28 11:22:48 +0800

    Update main_router.py

--- routers/main_router.py
+++ routers/main_router.py
@@ -1,20 +1,30 @@
+import asyncio
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import CommandStart
 from aiogram.filters import Command
+from aiogram.types import CallbackQuery
 from menus import *
 from twitter.tweet_main import *
 
 main_router = Router()
 @main_router.message(CommandStart())
 async def command_start(message: Message, state: FSMContext) -> None:
-    await message.answer("tweeter start feed back test.")
+        tweets = query_formart_tweet_md()
+        if len(tweets) > 0:
+            for tweet in tweets:
+                await message.answer(tweet)
+                await asyncio.sleep(5)
+        else:
+           await message.answer("start, no tweet, wait another cycle")
 
 @main_router.message(Command("kol_tweet_feed"))
 async def command_kol_tweet_feed(message: Message, state: FSMContext) -> None:
-        # tweets = generate_tweet_list()
-        # if len(tweets) > 0:
-        #     for tweet in tweets:
-        #         await message.answer(tweet)
-        await message.answer("tweeter feed back test.")
+        tweets = query_formart_tweet_md()
+        if len(tweets) > 0:
+            for tweet in tweets:
+                await message.answer(tweet)
+                await asyncio.sleep(5)
+        else:
+           await message.answer("no tweet, wait another cycle")
