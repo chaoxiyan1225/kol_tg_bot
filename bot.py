c6d2f843ba013f36656c20f721526adf033592a0
alex-yan <114714712+chaoxiyan1225@users.noreply.github.com>
2025-03-28 10:04:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
+from aiogram.client.bot import DefaultBotProperties
 from routers.main_router import main_router
 # from routers.menu_router import menu_router
 # from routers.state_router import state_router
@@ -10,7 +11,7 @@ from config import TELEGRAM_BOT_TOKEN
 
 
 async def main():
-    bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode=ParseMode.MARKDOWN)
+    bot = Bot(token=TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN))
     dp = Dispatcher()
     dp.include_router(main_router)
     # dp.include_router(menu_router)
@@ -28,3 +29,4 @@ async def main():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+    print("now bot start running")
