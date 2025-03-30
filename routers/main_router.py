8db12be1518564dd23f2d34bad189eae114d9dbd
alex-yan <114714712+chaoxiyan1225@users.noreply.github.com>
2025-03-30 09:56:01 +0800

    Update main_router.py

--- routers/main_router.py
+++ routers/main_router.py
@@ -14,9 +14,9 @@ main_router = Router()
 #async def debug_all_messages(message: Message):
 #    print(f"收到原始消息: {message.text}")
 
-@main_router.message(CommandStart())
-async def command_start(message: Message, state: FSMContext) -> None:
-        tweets = query_formart_tweet_md()
+async def __send_kol_messages__(message: Message, state: FSMContext) -> None:
+        user_id = message.from_user.id
+        tweets = query_formart_tweet_md(user_id)
         if len(tweets) > 0:
             for tweet in tweets:
                 await message.answer(tweet)
@@ -24,12 +24,11 @@ async def command_start(message: Message, state: FSMContext) -> None:
         else:
            await message.answer("start, no tweet, wait another cycle")
 
+
+@main_router.message(CommandStart())
+async def command_start(message: Message, state: FSMContext) -> None:
+    __send_kol_messages__(message, state)
+
 @main_router.message(Command("kol_tweet_feed"))
 async def command_kol_tweet_feed(message: Message, state: FSMContext) -> None:
-        tweets = query_formart_tweet_md()
-        if len(tweets) > 0:
-            for tweet in tweets:
-                await message.answer(tweet)
-                await asyncio.sleep(5)
-        else:
-           await message.answer("no tweet, wait another cycle")
+    __send_kol_messages__(message, state)
